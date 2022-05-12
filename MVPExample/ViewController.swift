a3fbfb7ca5fe5d16d5cc2202bdfc0bdeef52651e
Ruslan <ruslan.shtybaev@gmail.com>
2022-05-12 19:12:51 +0300

    Initial Commit

---
+++ MVPExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPExample
+//
+//  Created by Руслан Штыбаев on 12.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
